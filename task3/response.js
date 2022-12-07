bdffb6913a5e8a92114e6b3c235bac5437981869
Alexandr Korobov <devarko@sapo.pt>
2022-12-07 23:36:20 +0300

    Geolocation implemented

--- task3/response.js
+++ task3/response.js
@@ -1,10 +1,11 @@
+// Набор заготовок для ответов и форматирование времени и даты
+
 const date = new Date();
 
 const responseObj = {
     hi: 'Hello! How are you doing?',
     hey: "Hey! What's up?",
-    // today: date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear(),
-    today: new Date().toDateString(),
+    today: date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear(),
+    // today: new Date().toDateString(),
     time: date.getHours() + ':' + date.getMinutes(),
-    // time: new Date().toLocaleTimeString(),
 }
